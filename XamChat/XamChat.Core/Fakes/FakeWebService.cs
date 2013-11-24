8f00c1ddd66739d1bb0260ea739eb14f44247240
Jonathan Peppers <jonathan.peppers@gmail.com>
2013-11-23 20:49:33 -0600

    Updated Fake Service

--- XamChat/XamChat.Core/Fakes/FakeWebService.cs
+++ XamChat/XamChat.Core/Fakes/FakeWebService.cs
@@ -25,7 +25,7 @@ namespace XamChat.Core
         {
             await Sleep();
 
-            return new User { Id = 1, Username = username };
+			return new User { Id = "1", Username = username };
         }
 
         public async Task<User> Register(User user)
@@ -35,49 +35,49 @@ namespace XamChat.Core
             return user;
         }
 
-        public async Task<User[]> GetFriends(int userId)
+        public async Task<User[]> GetFriends(string userId)
         {
             await Sleep();
 
             return new[]
             {
-                new User { Id = 2, Username = "bobama" },
-                new User { Id = 3, Username = "bobloblaw" },
-                new User { Id = 4, Username = "gmichael" },
-                new User { Id = 5, Username = "scooper" },
+				new User { Id = "2", Username = "bobama" },
+				new User { Id = "3", Username = "bobloblaw" },
+				new User { Id = "4", Username = "gmichael" },
+				new User { Id = "5", Username = "scooper" },
+			};
+        }
+
+        public async Task<User> AddFriend(string userId, string username)
+        {
+            await Sleep();
+
+			return new User { Id = "6", Username = username };
+        }
+
+        public async Task<Conversation[]> GetConversations(string userId)
+        {
+            await Sleep();
+
+            return new[]
+            {
+                new Conversation { Id = "1", UserId = "2", Username = "bobama", LastMessage = "Hey!" },
+                new Conversation { Id = "2", UserId = "3", Username = "bobloblaw", LastMessage = "Have you seen that new movie?" },
+                new Conversation { Id = "3", UserId = "4", Username = "gmichael", LastMessage = "What?" },
             };
         }
 
-        public async Task<User> AddFriend(int userId, string username)
-        {
-            await Sleep();
-
-            return new User { Id = 6, Username = username };
-        }
-
-        public async Task<Conversation[]> GetConversations(int userId)
+        public async Task<Message[]> GetMessages(string conversationId)
         {
             await Sleep();
 
             return new[]
             {
-                new Conversation { Id = 1, UserId = 2, Username = "bobama", LastMessage = "Hey!" },
-                new Conversation { Id = 2, UserId = 3, Username = "bobloblaw", LastMessage = "Have you seen that new movie?" },
-                new Conversation { Id = 3, UserId = 4, Username = "gmichael", LastMessage = "What?" },
-            };
-        }
-
-        public async Task<Message[]> GetMessages(int conversationId)
-        {
-            await Sleep();
-
-            return new[]
-            {
-                new Message { Id = 1, ConversationId = conversationId, UserId = 2, Username = "bobama", Text = "Hey", Date = DateTime.Now.AddDays(-1).AddMinutes(-5) },
-                new Message { Id = 2, ConversationId = conversationId, UserId = 1, Username = "testuser", Text = "What's up?", Date = DateTime.Now.AddDays(-1) },
-                new Message { Id = 3, ConversationId = conversationId, UserId = 2, Username = "bobama", Text = "Have you seen that new movie?", Date = DateTime.Now.AddMinutes(-1) },
-                new Message { Id = 4, ConversationId = conversationId, UserId = 1, Username = "testuser", Text = "It's great!", Date = DateTime.Now.AddSeconds(-30) },
-                new Message { Id = 5, ConversationId = conversationId, UserId = 2, Username = "bobama", Text = "Cool", Date = DateTime.Now.AddSeconds(-15) },
+                new Message { Id = "1", ConversationId = conversationId, UserId = "2", Username = "bobama", Text = "Hey", Date = DateTime.Now.AddDays(-1).AddMinutes(-5) },
+                new Message { Id = "2", ConversationId = conversationId, UserId = "1", Username = "testuser", Text = "What's up?", Date = DateTime.Now.AddDays(-1) },
+                new Message { Id = "3", ConversationId = conversationId, UserId = "2", Username = "bobama", Text = "Have you seen that new movie?", Date = DateTime.Now.AddMinutes(-1) },
+                new Message { Id = "4", ConversationId = conversationId, UserId = "1", Username = "testuser", Text = "It's great!", Date = DateTime.Now.AddSeconds(-30) },
+                new Message { Id = "5", ConversationId = conversationId, UserId = "2", Username = "bobama", Text = "Cool", Date = DateTime.Now.AddSeconds(-15) },
             };
         }
 
